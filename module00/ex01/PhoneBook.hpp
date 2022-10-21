79d236295e55f8d2e2610f8e17421392984d6bae
El mehdi Elmohammady <eelmoham@e2r2p11.1337.ma>
2022-10-21 22:51:01 +0100

    done

--- module00/ex01/PhoneBook.hpp
+++ module00/ex01/PhoneBook.hpp
@@ -6,18 +6,18 @@
 /*   By: eelmoham <eelmoham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 22:21:50 by eelmoham          #+#    #+#             */
-/*   Updated: 2022/10/21 22:01:18 by eelmoham         ###   ########.fr       */
+/*   Updated: 2022/10/21 22:37:37 by eelmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
+#include "Contact.hpp"
 #include <iostream>
 #include <string>
 #include <cctype>
-#include <stdlib.h>
-#include "Contact.hpp"
+#include <cstdlib>
 
 int validIndex(std::string index);
 int isAllSpace(std::string pr);
