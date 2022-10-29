276928fe5fa581dfb8d7355e7ff630192d490378
Zero <mohammdymehdi@gmail.com>
2022-10-29 02:37:21 +0000

    harl is here

--- module01/ex05/Harl.cpp
+++ module01/ex05/Harl.cpp
@@ -12,22 +12,22 @@ Harl::~Harl()
 
 void Harl::debug( void )
 {
-    std::cout << "\"DEBUG\" level: Debug messages contain contextual information. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
+    std::cout << "Debug messages contain contextual information. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
 }
 
 void Harl::info( void )
 {
-    std::cout << "\"INFO\" level: Info messages are used to report information about the program's execution. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
+    std::cout << "Info messages are used to report information about the program's execution. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
 }
 
 void Harl::warning( void )
 {
-    std::cout << "\"WARNING\" level: Warning messages are used to report non-critical errors. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
+    std::cout << "Warning messages are used to report non-critical errors. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
 }
 
 void Harl::error( void )
 {
-    std::cout << "\"ERROR\" level: Error messages are used to report critical errors. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
+    std::cout << "Error messages are used to report critical errors. They are mostly used for problem diagnosis.\nExample: \"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\"" << std::endl;
 }
 
 void Harl::complain( std::string level )
