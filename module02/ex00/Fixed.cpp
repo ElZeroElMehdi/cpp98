57ff25c5e718fdd2a99020272666b65e6674a0b6
El mehdi Elmohammady <eelmoham@e1r2p13.1337.ma>
2022-11-18 02:21:44 +0100

    a night with imane

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -2,11 +2,13 @@
 
 Fixed::Fixed()
 {
+	std::cout << "Default constructor called\n";
 	this->fixedPoint = 0;
 }
 
 Fixed::Fixed(const Fixed &f)
 {
+	std::cout << "Copy constructor called\n";
 	*this = f;
 }
 
@@ -17,10 +19,12 @@ Fixed::~Fixed()
 
 int Fixed::getRawBits() const
 {
+	std::cout << "getRawBits member function called\n";
 	return this->fixedPoint;
 }
 
 Fixed & Fixed::operator=(Fixed const & obj){
+	std::cout << "Copy assignment operator called\n";
 	if (this != &obj)
 		this->fixedPoint = obj.getRawBits();
 	return (*this);
