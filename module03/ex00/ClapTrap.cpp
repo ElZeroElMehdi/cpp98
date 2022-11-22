5647ef423c6d5b63c1dd03c85e2224a49fef15cf
El mehdi Elmohammady <eelmoham@e1r3p4.1337.ma>
2022-11-22 21:49:20 +0100

    done]

--- module03/ex00/ClapTrap.cpp
+++ module03/ex00/ClapTrap.cpp
@@ -1,16 +1,64 @@
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap()
+{
+    std::cout << "Default constructor" << std::endl;
+    this->HitPoint = 10;
+    this->EnergyPoint = 10;
+    this->AttackDamage = 0;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &OldObj)
+{
+    std::cout << "Copy Constructor" << std::endl;
+    *this = OldObj;
+}
+
+ClapTrap & ClapTrap::operator=(const ClapTrap &rhs)
+{
+    std::cout << "assigment operator" << std::endl;
+    *this = rhs;
+    return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "destructor" << std::endl;
+}
+
 ClapTrap::ClapTrap(std::string& _name)
 {
+    std::cout << "Parameterized Constructor" << std::endl;
     this->Name = _name;
     this->HitPoint = 10;
     this->EnergyPoint = 10;
     this->AttackDamage = 0;
 }
 
-ClapTrap::ClapTrap()
+void ClapTrap::attack(const std::string& target)
 {
-    this->HitPoint = 10;
-    this->EnergyPoint = 10;
-    this->AttackDamage = 0;
+    if (!this->EnergyPoint || !this->HitPoint)
+        std::cout << this->Name <<" can\'t Attack , cause no HitPoint and No energyPint" << std::endl;
+    else
+    {
+        std::cout << "ClapTrap" << this->Name << "attacks" << target <<", causing" << this->AttackDamage << "points of damage!" << std::endl;
+        this->EnergyPoint--;
+    }
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    std::cout <<this->Name <<" cost" << amount << "points from HitPoint" << std::endl;
+    this->HitPoint -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (!this->EnergyPoint || !this->HitPoint)
+        std::cout << this->Name <<" can\'t Attack , cause no HitPoint and No energyPint" << std::endl;
+    else
+    {
+        this->EnergyPoint--;
+        this->HitPoint+=amount;
+    }
 }
---
+++ module03/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "ClapTrap.hpp";
+
+int main()
+{
+    ClapTrap s();
+    // std::string *name = new std::string("mehdi");
+    // ClapTrap *x = new ClapTrap("mehd");
+
+
+
+}
