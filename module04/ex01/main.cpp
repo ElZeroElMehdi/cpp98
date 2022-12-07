66fab0da3531100467e0cd814423a18663f9aeba
El mehdi Elmohammady <eelmoham@e1r4p11.1337.ma>
2022-12-07 20:02:22 +0100

    nev

---
+++ module04/ex01/main.cpp
@@ -0,0 +1,39 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongCat.hpp"
+#include "WrongAnimal.hpp"
+
+int main()
+{
+    const Animal *j = new Dog();
+    const Animal *i = new Cat();
+    delete j; // should not create a leak
+    delete i;
+    // ...
+    const Animal *N[10];
+    for (int i = 0; i < 10; i++)
+    {
+        if (i % 2)
+            N[i] = new Dog();
+        else
+            N[i] = new Cat();
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "****************************************" << std::endl;
+        std::cout << N[i]->getType() << std::endl;
+        N[i]->makeSound();
+        std::cout << "****************************************" << std::endl;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "|****************************************|" << std::endl;
+        delete N[i];
+        std::cout << "|****************************************|" << std::endl;
+    }
+
+    system("leaks Brain");
+    return 0;
+}
