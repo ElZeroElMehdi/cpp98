d0691c0943166a4e9ed97fbd8688461ff323e038
zero <mohammdymehdi@gmail.com>
2022-12-29 00:47:41 +0100

    fixed

--- module06/ex00/main2.cpp
+++ module06/ex00/main2.cpp
@@ -1,41 +1,59 @@
 #include <iostream>
-#include <climits>
 
-int checker(char *args)
+void to_char(std::string str)
 {
-    std::string str(args);
-
-    for( int i = 0 ; i < str.length(); i++)
+    try
     {
-        if ((str[i] < '0'  || str[i] > '9') && str[i] != 'f' && str[i] != '.')
-            return -1;
-        if ((str[i] == 'f' && str[i + 1]) || (str[i] == '.' && str[i + 1] && str[i + 1] == 'f'))
-            return -1;
+        int i = std::stoi(str);
+        char c = static_cast<char>(i);
+        if (std::isprint(c))
+            std::cout << "char :"<< c << std::endl;
+        else
+            std::cout << "non displayabl"<< std::endl;
     }
-    return 1;
+    catch(...)
+    {
+        std::cout << "char :Impossible" << std::endl;
+    }
+}
+
+void to_int(std::string str)
+{
+    try
+    {
+        int i = std::stoi(str);
+        std::cout << "int :" << i << std::endl;
+    }
+    catch(...)
+    {std::cout << "int :Impossible" << std::endl;}
+}
+
+void to_float(std::string str)
+{
+    try
+    {
+        float f = std::stof(str);
+        if (str == "nan" || str == "nanf")
+            std:
+        else if (f == std::stoi(str))
+            std::cout << "float :" << f << ".0f"<<std::endl;
+        else
+            std::cout << "float :" << f << "f"<<std::endl;
+    }
+    catch(...)
+    {std::cout << "float :Impossible" << std::endl;}
 }
 
 int main(int ac , char **args)
 {
     try
     {
-        if (ac == 2 && checker(args[1]))
+        if (ac == 2)
         {
-            double x = std::strtod(args[1], NULL);
-            if (static_cast<char>(x))
-                std::cout<< "char :" << static_cast<char>(x)<< std::endl;
-            else
-                std::cout<< "char: Non displayable"<< std::endl;
-            if (static_cast<int>(x) <= INT_MAX && static_cast<int>(x) >= INT_MIN)
-                std::cout<< "int :" << static_cast<int>(x) << std::endl;
-            if (static_cast<int>(x) == x)
-                std::cout<< "float :" << x <<".0f"<< std::endl;
-            else
-                std::cout<< "float :" << x <<"f"<< std::endl;
-            if (static_cast<int>(x) == x)
-                std::cout << "double :"<<x<<".0"<< std::endl;
-            else
-                std::cout << "double :"<<x<< std::endl;
+            std::string str(args[1]);
+            to_char(str);
+            to_int(str);
+            to_float(str);
         }
     }
     catch(const std::exception& e)
