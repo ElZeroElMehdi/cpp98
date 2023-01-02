2b6de37bc1aebddbc0f2f4aa001da4c5dfca560d
zero <mohammdymehdi@gmail.com>
2023-01-02 23:12:56 +0100

    add copy assigment and copy constructor

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -19,7 +19,30 @@ class Array
         {
             this->capacity_ = cap;
             this->data_ = new T[this->capacity_];
+            if (!this->data_)
+                exit(1);
         }
+        Array(const Array &_arr)
+        {
+            *this = _arr;
+        }
+
+        Array &operator=(const Array & copyAssig) {
+            this->capacity_ = copyAssig.capacity_;
+            this->data_ = new T[this->capacity_];
+            if (!this->data_)
+            {
+                exit(1);
+            }
+            int i = 0;
+            while (i < this->capacity_)
+            {
+                this->data_[i] = copyAssig.data_[i];
+                i++;
+            }
+            return (*this);
+        }
+
         T& operator[](size_t index)
         {
             if (index < 0 || index > this->capacity_)
