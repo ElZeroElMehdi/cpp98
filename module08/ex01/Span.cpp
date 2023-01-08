7539642731bbffe91d7effec691238f97d7520d0
El mehdi Elmohammady <eelmoham@e2r11p2.1337.ma>
2023-01-08 01:10:38 +0100

    addnumber

--- module08/ex01/Span.cpp
+++ module08/ex01/Span.cpp
@@ -1,15 +1,40 @@
-#include "span.h"
+#include "Span.hpp"
 
 Span::Span()
 {
-    v = NULL;
+    this->N = 0;
+    this->v = std::vector<int>(0);
 }
 
-Span::Spam(unsigned int n) : N(n)
+Span::Span(unsigned int n)
 {
-    this->v = new 
+    this->N = n;
+    this->v = std::vector<int>(n);
+}
+
+Span::Span(const Span &s)
+{
+    this->N = s.N;
+    this->v = s.v;
+}
+
+Span &Span::operator=(const Span &rhs)
+{
+    this->v = rhs.v;
+    this->N = rhs.N;
+    return (*this);
 }
 
 Span::~Span()
 {
+    std::cout << "dedtructor" << std::endl;
+}
+
+void Span::addNumber(int node)
+{
+    if (node == this->N)
+        throw std::exception();
+    this->N++;
+    this->v.resize(N);
+    this->v.push_back(node);
 }
--- module08/ex01/Span.hpp
+++ module08/ex01/Span.hpp
@@ -1,6 +1,7 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <iostream>
 #include <vector>
 
 class Span
@@ -11,11 +12,11 @@ class Span
     public:
         Span();
         Span(unsigned int n);
+        Span(const Span &s);
+        Span & operator=(const Span &rhs);
         ~Span();
-
+        void addNumber(int node);
+        
 };
-
-
-
 
 #endif
