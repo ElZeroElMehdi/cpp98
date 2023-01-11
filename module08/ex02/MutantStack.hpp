0524b945cd2cc86b2dd08cfe7bd2a3eda112be9e
zero <mohammdymehdi@gmail.com>
2023-01-11 19:03:31 +0100

    add begin end cbegin cend

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -3,14 +3,50 @@
 
 #include <iostream>
 #include <stack>
-template<class T>
-class MutantStack
+#include <algotithm>
+#include <vector>
+
+template<T>
+class MutantStack : public std::stack<T>
 {
     public:
-        MutantStack();
-        MutantStack(const MutantStack &cpy);
-        MutantStack &operator=(const MutantStack &rhs);
-        ~MutantStack();
+        MutantStack(){}
+
+        MutantStack(const MutantStack &cpy){
+            *this = cpy;
+        }
+
+        MutantStack &operator=(const MutantStack &rhs)
+        {
+            *this = rhs;
+            return (*this);
+        }
+
+        ~MutantStack(){}
+
+        typedef typename std::stack<T>::container_type::iterator it;
+        typedef typename std::stack<T>::container_type::const_iterator const_it;
+
+        it begin()
+        {
+            return (std::stack<T>::c.begin());
+        }
+
+        it end()
+        {
+            return (std::stack<T>::c.end());
+        }
+
+        const_it cbegin()
+        {
+            return (std::stack<T>::c.cbegin());
+        }
+
+        const_it cend()
+        {
+            return (std::stack<T>::c.cend());
+        }
+
 };
 
 #endif
